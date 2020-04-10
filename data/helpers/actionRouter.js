566087ee8d31eade2a02ba90b123aa4c7bda8ec9
chris296 <chris.rios296@gmail.com>
2020-04-10 16:02:28 -0700

    second commit

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -1,17 +1,30 @@
 const express = require('express');
 
 const Actions = require('./actionModel');
+const Projects = require('./projectModel');
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
-    Actions.insert(req.body)
-    .then(ac => {
-        res.status(201).json(ac)
+    console.log(req.body)
+    Projects.get(req.body.project_id)
+    .then(action => {
+        if (!action) {
+            res.status(400).json({message: 'invalid project id'})
+        } else {
+            Actions.insert(req.body)
+            .then(ac => {
+                res.status(201).json(ac);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({ message : 'creating action'})
+            })
+        }
     })
     .catch(err => {
         console.log(err);
-        res.status(500).json({ message: 'error adding action'})
+        res.status(500).json({message: 'error validating project id'})
     })
 })
 
